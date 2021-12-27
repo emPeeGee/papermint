0439bf9f23c2c2df75847c908622c3ac9f9abf46
Mihail Popovici <mipopovici@endava.com>
2021-12-27 18:14:13 +0200

    feat: Add theme toggler and context store

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Head from 'next/head'
 import NextLink from 'next/link'
-import { AppBar, Toolbar, Typography, Container, Link } from '@mui/material'
+import Cookies from 'js-cookie'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  Switch,
+} from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import useStyles from '../utils/styles'
+import { Store } from '../utils/store'
 
 export default function Layout({ children, title, description }) {
   const classes = useStyles()
+  const { state, dispatch } = useContext(Store)
+  const { darkMode } = state
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  })
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' })
+
+    const newDarkMode = !darkMode
+    Cookies.set('dark-mode', newDarkMode ? 'ON' : 'OFF')
+  }
 
   return (
     <>
@@ -15,31 +41,38 @@ export default function Layout({ children, title, description }) {
         {description && <meta name="description" content={description} />}
       </Head>
 
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>Papermint</Typography>
-            </Link>
-          </NextLink>
-
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>Papermint</Typography>
+              </Link>
             </NextLink>
-          </div>
-          <NextLink href="/login" passHref>
-            <Link>Login</Link>
-          </NextLink>
-        </Toolbar>
-      </AppBar>
 
-      <Container className={classes.main}>{children}</Container>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+            </div>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </Toolbar>
+        </AppBar>
 
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Paparmint</Typography>
-      </footer>
+        <Container className={classes.main}>{children}</Container>
+
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Paparmint</Typography>
+        </footer>
+      </ThemeProvider>
     </>
   )
 }
